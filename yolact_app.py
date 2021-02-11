598c717aee3847d56f82f5003391e61575fb7d4d
Jiancong Zheng <jiancong0204@outlook.com>
2021-02-11 13:58:18 +0100

    Add support for webcam

--- yolact_app.py
+++ yolact_app.py
@@ -22,6 +22,8 @@ class MainDialog(QDialog):
         self.setWindowTitle('YOLACT - GUI')
         self.validationFile = str()
         self.validationModel = str()
+        self.onlineFile = str()
+        self.onlineModel = str()
         self.benchmarkModel = str()
         self.trainConfig = 'yolact_base_config'
         self.batchSize = '1'
@@ -30,12 +32,14 @@ class MainDialog(QDialog):
         self.ui.pushButton_evaluate.setEnabled(False)
         self.ui.pushButton_benchmark.setEnabled(False)
         self.ui.radioButton_batchEvaluation.setChecked(False)
+        self.ui.pushButton_onlineTest.setEnabled(False)
         float_validator = QDoubleValidator(float("-inf"), float("inf"), 2, self)
         self.ui.lineEdit_scoreThreshold.setValidator(float_validator)
-
+        self.ui.lineEdit_onlineThreshold.setValidator(float_validator)
         int_validator = QIntValidator(1, 10000, self)
         self.ui.lineEdit_numImage.setValidator(int_validator)
-
+        int_validator = QIntValidator(0, 99, self)
+        self.ui.lineEdit_camIndex.setValidator(int_validator)
         # Training
         self.t = TrainingThread()
         self.t.signalForText.connect(self.updateTextBrowser)
@@ -65,16 +69,18 @@ class MainDialog(QDialog):
         self.ui.horizontalSlider_scoreThreshold.valueChanged.connect(self.sliderEditLineEdit)
 
         # Online Test
-        ## Video test
         self.d = DisplayThread()
         self.d.signalForDisplay.connect(self.displayImage)
         self.ui.pushButton_onlineTest.clicked.connect(self.triggerOnlineTest)
+        self.ui.pushButton_onlineModel.clicked.connect(self.chooseOnlineModel)
+        self.ui.pushButton_onlineVideo.clicked.connect(self.chooseOnlineFile)
+        self.ui.lineEdit_onlineThreshold.editingFinished.connect(self.onlineThresholdLimit)
 
         # Clear
-        self.ui.pushButton_reset.clicked.connect(self.clearWindow)
+        self.ui.pushButton_reset.clicked.connect(self.resetWindow)
         self.ui.pushButton_clearTerminal.clicked.connect(self.clearTerminal)
         self.ui.pushButton_terminateThread.clicked.connect(self.terminateThread)
-        self.ui.pushButton_terminateThread.setEnabled(False)
+        self.clearWindow()
 
     def displayImage(self):
         curr_path = os.getcwd()
@@ -86,7 +92,7 @@ class MainDialog(QDialog):
         self.b.terminate()
         self.t.terminate()
         self.e.terminate()
-        self.ui.pushButton_terminateThread.setEnabled(False)
+        self.clearWindow()
 
     def updateTextBrowser(self, text):
         cursor = self.ui.textBrowser_terminal.textCursor()
@@ -109,7 +115,6 @@ class MainDialog(QDialog):
         batch_size = self.batchSize
         args = train_script.parse_args(['--config=' + config, '--batch_size=' + batch_size])
         self.t = TrainingThread(args)
-        self.ui.pushButton_terminateThread.setEnabled(True)
         self.t.start()
         loop = QEventLoop()
         QTimer.singleShot(2000, loop.quit)
@@ -118,11 +123,10 @@ class MainDialog(QDialog):
     def triggerBenchmark(self):
         # print(self.benchmarkModel)
         self.b = BenchmarkThread(self.benchmarkModel, self.benchmarkNumImage)
-        self.clearWindow()
-        self.ui.pushButton_terminateThread.setEnabled(True)
         self.b.start()
         loop = QEventLoop()
         QTimer.singleShot(2000, loop.quit)
+
 
     def triggerEvaluation(self):
         image = self.validationFile
@@ -141,20 +145,19 @@ class MainDialog(QDialog):
             self.ui.display.setPixmap(pixmap)
             return
 
-        self.clearWindow()
 
         eval_script.parse_args(['--trained_model=' + model])
         self.e = EvaluationThread()
-        self.ui.pushButton_terminateThread.setEnabled(True)
         self.e.start()
+        self.ui.pushButton_terminateThread.setEnabled(True)
         loop = QEventLoop()
         QTimer.singleShot(2000, loop.quit)
 
     def chooseTrainedModel(self, FilePath):
         model = QtWidgets.QFileDialog.getOpenFileName(self,  "Select your model", "./weights")[0]
         if model == '':
-            self.ui.pushButton_selectModel.setText('Select model')
-            self.ui.pushButton_evaluate.setEnabled(False)
+            self.ui.pushButton_onlineModel.setText('Select model')
+            self.ui.pushButton_onlineTest.setEnabled(False)
             return
 
         pos = model.find('yolact-gui')
@@ -173,13 +176,74 @@ class MainDialog(QDialog):
 
         if post != 'htp':
             self.ui.display.setText('Selected file is not valid model file (.pth)!')
-            self.ui.pushButton_evaluate.setEnabled(False)
+            self.ui.pushButton_onlineTest.setEnabled(False)
             return
 
         self.ui.pushButton_selectModel.setText(model[pos:])
         self.validationModel = model
         if self.validationModel != '' and self.validationFile != '':
-            self.ui.pushButton_evaluate.setEnabled(True)
+            self.ui.pushButton_onlineTest.setEnabled(True)
+
+    def chooseOnlineModel(self, FilePath):
+        model = QtWidgets.QFileDialog.getOpenFileName(self, "Select your model", "./weights")[0]
+        if model == '':
+            self.ui.pushButton_onlineModel.setText('Select model')
+            self.ui.pushButton_onlineTest.setEnabled(False)
+            return
+
+        pos = model.find('yolact-gui')
+        model = model[pos + 11:]
+        pos = len(model)
+        for ch in model[::-1]:
+            if ch == '/':
+                break
+            pos = pos - 1
+
+        post = str()
+        for ch in model[::-1]:
+            if ch == '.':
+                break
+            post += ch
+
+        if post != 'htp':
+            self.ui.display.setText('Selected file is not valid model file (.pth)!')
+            self.ui.pushButton_onlineModel.setEnabled(False)
+            return
+
+        self.ui.pushButton_onlineModel.setText(model[pos:])
+        self.onlineModel = model
+        if self.onlineModel != '' and self.onlineFile != '':
+            self.ui.pushButton_onlineTest.setEnabled(True)
+
+    def chooseOnlineFile(self):
+        video = QtWidgets.QFileDialog.getOpenFileName(self, "Select your video", "./video")[0]
+        if video == '':
+            self.ui.pushButton_onlineVideo.setText('Select image')
+            self.ui.pushButton_onlineTest.setEnabled(False)
+            return
+        pos = video.find('yolact-gui')
+        video = video[pos + 11:]
+
+        pos = len(video)
+        for ch in video[::-1]:
+            if ch == '/':
+                break
+            pos = pos - 1
+
+        post = str()
+        for ch in video[::-1]:
+            if ch == '.':
+                break
+            post += ch
+
+        if post != '4pm':
+            self.ui.display.setText('Selected file is not a valid video (.mp4)!')
+            self.ui.pushButton_onlineTest.setEnabled(False)
+            return
+        self.ui.pushButton_onlineVideo.setText(video[pos:])
+        self.onlineFile = video
+        if self.onlineModel != '' and self.onlineFile != '':
+            self.ui.pushButton_onlineTest.setEnabled(True)
 
     def chooseBenchmarkModel(self, FilePath):
         model = QtWidgets.QFileDialog.getOpenFileName(self,  "Select your model", "./weights")[0]
@@ -246,34 +310,72 @@ class MainDialog(QDialog):
         '''
         python eval.py --trained_model=weights/yolact_base_54_800000.pth --score_threshold=0.25 --top_k=15 --video_multiframe=4 --video=video/demo.mp4
         '''
-        model = 'weights/yolact_base_54_800000.pth'
-        score_threshold = 0.25
+        model = self.onlineModel
+        score_threshold = self.ui.lineEdit_onlineThreshold.text()
         video_multiframe = 4
-        video = 'video/demo.mp4'  # :results/tmp_video.mp4
-
-        eval_script.parse_args(['--trained_model=' + model, '--score_threshold=' + str(score_threshold),
+        video = self.onlineFile  # :results/tmp_video.mp4
+        if self.ui.radioButton_webcam.isChecked():
+            webcam = self.ui.lineEdit_camIndex.text()
+            eval_script.parse_args(['--trained_model=' + model, '--score_threshold=' + score_threshold,
+                                    '--top_k=15', '--video_multiframe=' + str(video_multiframe), '--video=' + webcam])
+        else:
+            eval_script.parse_args(['--trained_model=' + model, '--score_threshold=' + score_threshold,
                                 '--top_k=15', '--video_multiframe=' + str(video_multiframe), '--video=' + video])
         self.e = EvaluationThread()
         self.e.start()
         self.d.start()
-
+        self.ui.pushButton_terminateThread.setEnabled(True)
 
         loop = QEventLoop()
         QTimer.singleShot(2000, loop.quit)
 
+    def resetWindow(self):
+        self.ui.display.clear()
+        self.ui.textBrowser_terminal.clear()
+        self.validationFile = str()
+        self.validationModel = str()
+        self.benchmarkModel = str()
+        self.ui.pushButton_selectModel.setText('Select model')
+        self.ui.pushButton_selectImage.setText('Select image')
+        self.ui.pushButton_onlineVideo.setText('Select video')
+        self.ui.pushButton_evaluate.setEnabled(False)
+        self.ui.horizontalSlider_scoreThreshold.setValue(15)
+        self.ui.lineEdit_scoreThreshold.setText('0.15')
+        self.ui.lineEdit_onlineThreshold.setText('0.15')
+        self.ui.lineEdit_camIndex.setText('0')
+        self.ui.radioButton_batchEvaluation.setChecked(False)
+
+        self.ui.pushButton_selectModel_2.setText('Select model')
+        self.benchmarkNumImage = 1000
+        self.ui.pushButton_benchmark.setEnabled(False)
+        self.ui.lineEdit_numImage.setText('1000')
+        self.ui.horizontalSlider_numImage.setValue(1000)
+
+        self.trainConfig = 'yolact_base_config'
+        self.batchSize = '1'
+        self.ui.lineEdit_batchSize.setText('1')
+        self.ui.lineEdit_trainConfig.setText('yolact_base_config')
+
+        # delete tmp result
+        curr_path = os.getcwd()
+        delete_file = curr_path + '/results/tmp_res.jpg'
+        if os.path.exists(delete_file):
+            os.remove(delete_file)
 
     def clearWindow(self):
         self.ui.display.clear()
-        self.ui.textBrowser_terminal.clear()
 
         self.validationFile = str()
         self.validationModel = str()
         self.benchmarkModel = str()
         self.ui.pushButton_selectModel.setText('Select model')
         self.ui.pushButton_selectImage.setText('Select image')
+        self.ui.pushButton_onlineVideo.setText('Select video')
         self.ui.pushButton_evaluate.setEnabled(False)
         self.ui.horizontalSlider_scoreThreshold.setValue(15)
         self.ui.lineEdit_scoreThreshold.setText('0.15')
+        self.ui.lineEdit_onlineThreshold.setText('0.15')
+        self.ui.lineEdit_camIndex.setText('0')
         self.ui.radioButton_batchEvaluation.setChecked(False)
 
         self.ui.pushButton_selectModel_2.setText('Select model')
@@ -333,6 +435,19 @@ class MainDialog(QDialog):
         self.benchmarkNumImage = value
         self.ui.lineEdit_numImage.setText(str(value))
 
+    def onlineThresholdLimit(self):
+        value = self.ui.lineEdit_onlineThreshold.text()
+        value = float(value)
+        value = format(value, '.2f')
+        value = float(value)
+        if value < 0:
+            value = 0
+        if value > 0.99:
+            value = 0.99
+        self.ui.lineEdit_onlineThreshold.setText(str(value))
+
+
+
 
 class TrainingThread(QThread):
     signalForText = pyqtSignal(str)
@@ -364,7 +479,7 @@ class BenchmarkThread(QThread):
 
     def run(self):
         eval_script.parse_args(['--trained_model=' + self.model, '--benchmark', '--max_images=' + str(self.num)])
-        eval_script.perform(eval_script.args)
+        eval_script.perform()
 
 
 class EvaluationThread(QThread):
